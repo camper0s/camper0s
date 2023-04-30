f230a78549affb3101c672ef32b1b22656195cd1
camper0s <132194206+camper0s@users.noreply.github.com>
2023-04-30 21:52:05 +0200

    Update esp8266_and_p5js_Processing_via_wifi.ino

--- esp8266_and_p5js_Processing_via_wifi.ino
+++ esp8266_and_p5js_Processing_via_wifi.ino
@@ -3,7 +3,6 @@
 //////////////////////////////////////////////////////////////////////////html
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML><html>
- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/p5.js"></script>
    <script type="text/javascript">
 /////////////////////////////////////////////////////////////////////Processing P5.js
